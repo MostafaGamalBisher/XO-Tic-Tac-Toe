6ac946c359c601e9cfca236e3d8c20472facbaaf
mostafagamalbisher <mostafagamalbisher@outlook.com>
2026-03-25 16:55:59 +0300

    starting writing the game logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,44 @@
-// ============================================================
-// MAIN ENTRY POINT
-// Import modules here and initialize the application.
-// ============================================================
+const currentPlayer = 'X';
+const NUMBER_OF_ROWS = 3;
+const turns = NUMBER_OF_ROWS ** 2;
+const turnsCounter = 0;
+const container = document.querySelector('.container');
+
+document.documentElement.style.setProperty('--grid-rows', NUMBER_OF_ROWS);
+
+const cellClickHandler = (event, index) => {
+  const row = Math.floor(index / NUMBER_OF_ROWS);
+  const column = index % NUMBER_OF_ROWS;
+
+  console.log({ row });
+  console.log({ column });
+};
+
+const createCell = (index) => {
+  const cell = document.createElement('div');
+  const valueSpan = document.createElement('span');
+
+  cell.classList.add('cell');
+  cell.setAttribute('role', 'button');
+  cell.setAttribute('tabindex', index + 1);
+  valueSpan.classList.add('value');
+
+  cell.appendChild(valueSpan);
+
+  cell.addEventListener('click', (event) => cellClickHandler(event, index));
+
+  return cell;
+};
+
+const createBoard = () => {
+  const board = document.createElement('div');
+
+  board.classList.add('board');
+
+  for (let i = 0; i < NUMBER_OF_ROWS ** 2; i++) {
+    board.appendChild(createCell(i));
+  }
+  container.insertAdjacentElement('afterbegin', board);
+};
+
+createBoard();
